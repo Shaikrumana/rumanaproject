bef8cadd5c410e8383a0ef1fa87863bbd73cc0bf
Shaikrumana <119430797+Shaikrumana@users.noreply.github.com>
2022-11-29 21:35:06 +0530

    Update backend.tf

--- DevOps_Project_2/Terraform/backend.tf
+++ DevOps_Project_2/Terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "rumanadevopsproject-2-terraform-statefile"
-    key = "jenkinskeypair.pem"
+    key = "server_name/statefile"
     region = "ap-south-1"
   }
 }  
